f35beaf37a31942929b4806b42a816af4f094c24
nikolayhotim1 <hotim_nikolay@mail.ru>
2024-02-09 00:12:23 +0300

    feature: connect mysql to prisma

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import { db } from "@/lib/db";
+import { db } from '@/lib/db'
 
 export default async function Home() {
-  const tasks = await db.task.findMany();
+	const tasks = await db.task.findMany({
+		where: { title: { contains: 'N' } }
+	})
 
-  return (
-    <div>
-      {tasks.map((task) => (
-        <div key={task.id}>{task.title}</div>
-      ))}
-    </div>    
-  );
+	return (
+		<div>
+			{tasks.map(task => (
+				<div key={task.id}>{task.title}</div>
+			))}
+		</div>
+	)
 }
